3812a8e411587f801099cbcc5d6d7fcd506cb85e
dev-coda <david.correav@gmail.com>
2022-10-04 21:58:47 -0500

    fix: arregla los problemas de compilación

--- components/Counter.js
+++ components/Counter.js
@@ -1,17 +1,24 @@
 import React,{useState, useEffect} from "react";
 
+//Recibimos el valor inicial del contador como props
+const Counter = ({ startNumber }) => {
+  //Definimos un numero inicial para el contador y lo guardamos en una variable con estado (stateful). Aca se lee el valor de startNumber, y como ya se asignó, cuando startNumber cambie, count no va a cambiar, ni su valor inicial.
+  const [count, setCount] = useState(startNumber);
 
-const Counter = ({startNumber}) => {
-  const [count, setCount] = useState(2);
+  //Handler para el contador de incremento
   const handleClick = () => {
     const previousCount = count
     setCount(previousCount+1)
     
   };
+  //El useEffect nos muestra el count en consola cada rerender.
   useEffect(() => {console.log(count)}, [count])
   return (
     <div>
-      <p>{count}{startNumber}</p>
+            {/* Vemos que una vez que este componente carga, el valor inicial de count no cambia, aunque startNumber cambie. Esto es porque ese componente solo se está leyendo como valor inicial */ }
+      <p> Cuenta : {count} </p>
+            {/* El valor inicial se muestra aca, aunque el estado de este lo maneja el padre */ }
+       <p>Valor Inicial: {startNumber}</p>
         <button id="button" onClick={handleClick}>+</button>
     </div>
   );
--- pages/morris.js
+++ pages/morris.js
@@ -1,22 +1,30 @@
 import React from 'react'
 import { useState } from 'react'
-import Counter from '../components/counter'
+import Counter from '../components/Counter'
 
 
-const morris = () => {
+const Morris = () => {
+  
+    //Definimos un numero inicial para el contador y lo guardamos en una variable con estado (stateful)
+  const [startNumber, setStartNumber] = useState(8)
+  
+  //Esto es para el toggler del principio
     const [toggleStatus, setToggleStatus] = useState(true)
-    const numero = 5
     const handleClick = () => {       
         setToggleStatus(!toggleStatus)
     }
 
   return (
     <>
+      {/* El toggler */ }
     <p>{toggleStatus ? "trici" : "el buen morris"}</p>
-        <a onClick={handleClick}>cambio!</a>
-        <Counter startNumber={numero}/>
+      <a onClick={handleClick}>cambio!</a>
+      {/* Creamos un botón que cambia al azar de 1 a 100 el valor de startNumber */ }
+      <button onClick={() => setStartNumber(Math.floor(Math.random()*100))}>Cambiar número inicial</button>
+          {/* Llamamos el componente pasandole startNumber como props */ }
+      <Counter startNumber={startNumber} />
     
     </>
   )
 }
-export default morris
+export default Morris
