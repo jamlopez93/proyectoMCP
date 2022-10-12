d2396b7dc3f1d51c0daf2710bfd1e1e802e6ee2e
Mauricio López <maurilo934@hotmail.com>
2022-10-12 12:38:24 -0500

    añadi el resto de cosas faltantes en el personaje, reorganice unas cositas para leerlo mas facil asi como esta pero creo que hay que reacomodarlas para que sea mas aprecido al diagrama de carta

--- pages/Main.js
+++ pages/Main.js
@@ -25,31 +25,29 @@ const personaje = {
     energy: 4,
     mystical: 3,
   },
-  attacks: [
-    {
-      attackType: "Physical",
-      attackName: "Spider strike",
-      isBeam: false,
-      isArea: false,
-      range: 3,
-      strength: 5,
-      powerCost: 0,
-      abilities: ["dis is an attack"],
-    },
-  ],
+  attacks: {
+    attackType: "Physical",
+    attackName: "Spider strike",
+    isBeam: false,
+    isArea: false,
+    range: 3,
+    strength: 5,
+    powerCost: 0,
+    abilities: ["dis is an attack"],
+  },
   leadership: {
     name: "Friendly",
     affiliation: "Web warriors",
     leadershipText: "dis is a leadership",
   },
-  superPowers: [
+  superPowers: 
     {
       powerType: "Reactive",
       name: "Web line",
       cost: "2",
       powerText: "dis is text",
     },
-  ],
+  
 };
 const main = () => {
   return (
@@ -58,7 +56,56 @@ const main = () => {
         <h3>{personaje.name}</h3>
         <p className="text-[8px] text-slate-600 uppercase">{personaje.alias}</p>
       </div>
-      <div>{personaje.statLine.map((stat)=>{return <p>{stat.name} : {stat.value}</p>})}</div>
+      <div>
+        {/* stats del personaje */}
+        {personaje.statLine.map((stat) => {
+          return (
+            <p>
+              {stat.name}: {stat.value}
+            </p>
+          );
+        })}
+      </div>
+      <br></br>
+      <div>
+        {/* Defensas del personaje */}
+        <p>Physical: {personaje.defense.physical}</p>
+        <p>Energy: {personaje.defense.energy}</p>
+        <p>Mystical: {personaje.defense.mystical}</p>
+      </div>
+      <br></br>
+      {/* div de ataques */}
+      <div>
+        {/* un ataque */}
+        <div>
+          <h4>{personaje.attacks.attackName}</h4>
+          <p>Type: {personaje.attacks.attackType}</p>
+          <p>
+            {" "}
+            Range: {personaje.attacks.range} Strength:{" "}
+            {personaje.attacks.strength} Cost: {personaje.attacks.powerCost}
+          </p>
+          <p>{personaje.attacks.abilities}</p>
+        </div>
+      </div>
+      <br></br>
+      {/* superpoderes y liderazgo */}
+      <div>
+        {/*liderazgo */}
+        <div>
+          <h4>
+            {personaje.leadership.name} {personaje.leadership.affiliation}
+          </h4>
+          <p>{personaje.leadership.leadershipText}</p>
+        </div>
+        <br></br>
+        {/* superpoderes */}
+        <div>
+          <h4>{personaje.superPowers.name} </h4>
+          <p>Type: {personaje.superPowers.powerType} cost: {personaje.superPowers.cost}</p>
+          <p>{personaje.superPowers.powerText}</p>
+        </div>
+      </div>
     </section>
   );
 };
